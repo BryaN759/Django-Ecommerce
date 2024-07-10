18f097dfbcbf228c0d76f9e70138801547df3b96
BryaN759 <khaledmushahed@gmail.com>
2024-07-10 13:46:31 +0600

    Resolve merge conflicts

--- KenaKata/settings.py
+++ KenaKata/settings.py
@@ -161,7 +161,7 @@ STATIC_URL = '/static/'
 #     'KenaKata/static',
 # ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # media files configuration
 
